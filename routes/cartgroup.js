6df7fcb0e9ce43ac7b43606920fb214578c7edb4
Wanjiru-Natalie <nataliewanjiru19@gmail.com>
2025-03-25 17:51:54 +0300

    moved the getUserId function to the javascript section

--- routes/cartgroup.js
+++ routes/cartgroup.js
@@ -5,6 +5,8 @@ const CartGroup = require("../models/cartGroup");
 const mongoose = require('mongoose');
 const User = require("../models/user")
 const jwt = require('jsonwebtoken');
+const { getUserId } = require("../public/javascripts/getUserfunction");
+
 
 const app = express();
 const router = express.Router();
@@ -27,33 +29,6 @@ io.on("connection", (socket) => {
   });
 });
 
-function getUserId(req) {
-    return new Promise((resolve, reject) => {
-      // Extract token
-      const token = req.headers.authorization?.split(" ")[1];
-      if (!token) {
-        return reject({ status: 401, message: "Authentication token missing" });
-      }
-  
-      // Verify token
-      jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-        if (err) {
-          return reject({ status: 403, message: "Invalid token" });
-        }
-  
-        try {
-          const user = await User.findOne({ email: decoded.email }).select("-password");
-          if (!user) {
-            return reject({ status: 404, message: "User not found" });
-          }
-  
-          resolve(user.id); 
-        } catch (error) {
-          reject({ status: 500, message: error.message });
-        }
-      });
-    });
-  }
   
 
 
